cf8ff36a0cb70ada202a8ebfe00aa2d1a4051054
kkjeer <kkjeer.wellesley.edu>
2025-03-27 14:45:58 -0500

    initial files

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+# kkjeerAppTesting release notes
+
+=========================================
+
+## 0.0.0
+
+- Module created by kb-sdk init
---
+++ lib/kkjeerAppTesting/kkjeerAppTestingImpl.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#BEGIN_HEADER
+#END_HEADER
+
+
+class kkjeerAppTesting:
+    '''
+    Module Name:
+    kkjeerAppTesting
+
+    Module Description:
+    A KBase module: kkjeerAppTesting
+    '''
+
+    ######## WARNING FOR GEVENT USERS ####### noqa
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    ######################################### noqa
+    VERSION = "0.0.1"
+    GIT_URL = "https://github.com/kkjeer/kkjeerAppTesting.git"
+    GIT_COMMIT_HASH = "HEAD"
+
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
+        #END_CONSTRUCTOR
+        pass
+
+
+    def run_kkjeerAppTesting(self, ctx, params):
+        """
+        This example function accepts any number of parameters and returns results in a KBaseReport
+        :param params: instance of mapping from String to unspecified object
+        :returns: instance of type "ReportResults" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN run_kkjeerAppTesting
+        #END run_kkjeerAppTesting
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method run_kkjeerAppTesting return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+    def status(self, ctx):
+        #BEGIN_STATUS
+        returnVal = {'state': "OK",
+                     'message': "",
+                     'version': self.VERSION,
+                     'git_url': self.GIT_URL,
+                     'git_commit_hash': self.GIT_COMMIT_HASH}
+        #END_STATUS
+        return [returnVal]
